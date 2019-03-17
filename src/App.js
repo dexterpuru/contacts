e1d9c276f514d334cb87fa524b5a3c50a65a95f2
dexterpuru <pratyaksh2000saini@google.com>
2019-03-17 15:37:38 +0530

    Contacts-app-react

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ListContacts from './ListContacts'
+
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+
+removeContact = (contact) => {
+  this.setState((state) => ({
+    contacts: state.contacts.filter((c) => c.id !== contact.id)
+  }))
+}
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ListContacts contacts={this.state.contacts} />
       </div>
-    );
+    )
   }
 }
 
